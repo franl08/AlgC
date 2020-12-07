70d868cfc4346a952a9d8ac5f91d840213f9281c
franl08 <a93202@alunos.uminho.pt>
2020-12-07 15:11:52 +0000

    Adicionada aula teórica 3

--- Grafos/teoricas.c
+++ Grafos/teoricas.c
@@ -128,7 +128,7 @@ int haArestaL (GrafoL g, int o, int d)
 
 // Função que procura um caminho (se d é alcançável a partir de o)
 
-int haCaminhoAux (GrafoL g, int o, int d, int vis[])
+int procuraAux (GrafoL g, int o, int d, int vis[])
 {
     ListaAdj x;
     vis[o] = 1; // não iniciamos mais procuras a partir da origem
@@ -137,19 +137,99 @@ int haCaminhoAux (GrafoL g, int o, int d, int vis[])
         // Existe uma aresta desde o até x->destino com peso x->peso
         if (vis[x->destino] == 0 // ainda não procuramos a partir deste ponto
             && 
-            haCaminhoAux(g, x->destino, d, vis)) return 1;
+            procuraAux(g, x->destino, d, vis)) return 1;
     }
     return 0;
 }
 // A Complexidade desta função é dada por T(V,E) = O(V + E)
 
-int haCaminho (GrafoL g, int o, int d)
+int procura (GrafoL g, int o, int d)
 {
     int vis[V]; // a dimensão do array vis é o número de vértices, pois, é utilizado para testar por quais vértices já passamos
     int i;
     for (i = 0; i < V; i++) vis[i] = 0;
-    return (haCaminhoAux(g,o,d,vis));
+    return (procuraAux(g,o,d,vis));
 }
-/* A Complexidade desta função é dada por T(V,E) = V + T_haCaminhoAux(V,E) = V + V + E = O(V + E)
+/* A Complexidade desta função é dada por T(V,E) = V + T_procuraAux(V,E) = V + V + E = O(V + E)
    Podemos considerar esta função linear, pois percorremos toda a estrutura apenas uma vez.
 */
+
+// Dado um vértice, dizemos que a ligação entre esses vértices e os seus pontos alcançáveis chama-se travessia.
+
+// Função que analisa a travessia em profundidade e a sua função auxiliar
+
+int travessiaDepthFirstAux (GrafoL g, int o, int vis[])
+{
+    int r;
+    ListaAdj x;
+    vis[o] = 1;
+    r = 1;
+    for (x = g[o]; x != NULL; x = x->prox)
+       if (vis[x->destino] == 0)
+          r += travessiaDepthFirstAux (g, x->destino, vis);
+    return r;
+}
+
+int travessiaDepthFirst (GrafoL g, int o)
+{
+    int v[V];
+    int i;
+    for (i = 0; i < V; i++) v[i] = 0;
+    return (travessiaDepthFirstAux(g, o, v));   // retorna o número de vértices alcançáveis a partir de o
+}
+
+/* Outro modo de travessia de um grafo é a travessia em largura.
+Para podermos criar esta função devemos compreender o conceito de Algoritmos Baseados em Orla.
+Para a definição destes algoritmos, devemos considerar que os vértices dos grafos estão divididos em 3 zonas:
+   1. Não visitados (Branco) -> não sabemos nada sobre eles;
+   2. Orla (Cinzento) -> os próximos elementos a serem visitados;
+   3. Visitados (Preto) -> elementos que já foram visitados e não o voltarão a ser.
+Podemos considerar que dento destes algoritmos existe o seguinte Invariante:
+   "Todos os nodos na orla são adjacentes a algum vértice visitado."
+no entanto existe uma exceção para este invariante.
+
+Para estes algoritmos devemos:
+   1. inicializar todos os vértices não visitados (Brancos)
+   2. inicializar a orla, da seguinte forma:
+      while (houver candidatos){
+          -> escolher vértice v da orla (Cinzento)
+          -> v passa para os visitados (Preto)
+          for (x in adjacentes de v)
+             acrescentar x à orla
+      }
+
+*/
+
+// Faremos, então, agora a função que analisa a travessia em largura
+
+#define Branco 0
+#define Cinzento 1
+#define Preto 2
+
+int travessiaBreadthFirst (GrafoL g, int o)
+{
+    int cor[v];
+    int orla[v];
+    int i, v, r;
+    int inicio0, fim0;                // estas duas variáveis são as suficientes para definir uma Queue Orla
+    ListaAdj x;
+    for (i = 0; i < v; i++) cor[i] = Branco;
+    inicio0 = fim0 = 0;            // queue vazia
+    cor[o] = Cinzento;
+    orla[fim0++] = o;            // enqueue (orla, o)
+    tamOrla = 1;
+    while (fim0 != inicio0){   // tamanho da orla > 0
+        v = orla[inicio0++];  // dequeue (orla)
+        cor[v] = Preto;
+        r++;
+        for (x = g[v]; x != NULL; x = x->prox){
+            // existe uma aresta de v e destino x->destino
+            if (cor[x->destino] == Branco){
+                orla[fim0++] = x->destino;   // enqueue (orla, x->destino)
+                cor[x->destino] = Cinzento;
+            }
+        }
+    
+    }
+    return r;
+}
